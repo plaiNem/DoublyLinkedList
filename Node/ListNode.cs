d103ec1c496196d039f9e71de4394b04487182f0
Arseniy Lagunovskiy <60578938+plaiNem@users.noreply.github.com>
2022-05-02 13:04:16 +0300

    changes

--- Node/ListNode.cs
+++ Node/ListNode.cs
@@ -4,14 +4,14 @@
     {
         internal class Node
         {
+            public Node? Next { get; set; }
+            public Node? Previous { get; set; }
+            public T Data { get; set; }
             public Node(T data)
             {
                 Next = null;
                 Data = data;
             }
-            public  Node? Next { get; set; }
-            public Node? Previous { get; set; }
-            public T Data { get; set; }
         }
     }
 }
